185e940702384d5e5722426c92d7d83df521d441
Malek Mechi <malek.mechi.ext@aleia.com>
2024-01-23 22:54:24 +0100

    created container deploy script

--- container/components/MarketingApp.js
+++ container/components/MarketingApp.js
@@ -4,7 +4,6 @@ import React, { useRef, useEffect } from 'react';
 export default ()=>{
 
        const ref = useRef(null);
-console.log(ref)
        useEffect(()=>{
 mount(ref.current)
        }, [])
